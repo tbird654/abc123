b0c2a551dc160d5b92cc41d6ecb9c3c3eabcd96c
Dustin <dustin.thornborrow@getliveedit.com>
2018-02-21 19:44:28 -0600

    Test Commit and push

---
+++ scss/config.rb
@@ -0,0 +1,14 @@
+preferred_syntax = :sass
+http_path = '/'
+
+
+
+css_dir = '../css'
+sass_dir = '.'
+
+
+
+relative_assets = true
+line_comments = true
+output_style = :compressed
+sourcemap = true
